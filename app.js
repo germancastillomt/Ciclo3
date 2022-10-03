4cd51f63fb6c72c5fb89207a6319bc497faf1dae
germancastillomt <german.castillo.mt@usa.edu.co>
2022-10-02 19:29:11 -0500

    MI PRIMERA VERSION 1.0

---
+++ app.js
@@ -0,0 +1,13 @@
+//console.log('hello world');
+//var time= Date();
+//alert(tiem.getHours() + ":" + time.gerMinutes() + ":" + time.getSeconds());
+
+//alert("Esto es un msj en una ventan!");
+//document.write("la informacion se muestra dentro de la pagina WEB");
+//document.write("<h3>ESTE ES MI TITULO 3</h3>");
+
+
+
+
+
+
